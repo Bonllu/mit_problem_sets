2d9fbb0ab52f44329a59d553e94ae087521b2397
Bonllu <151839401+Bonllu@users.noreply.github.com>
2024-07-09 12:39:30 +0200

    Update new_hand.py

--- new_hand.py
+++ new_hand.py
@@ -7,6 +7,12 @@ hand = {'a': 1, 'q': 1, 'l': 2, 'm': 1, 'u':1, 'i':1}
 word = 'quail'
 
 def update_hand(hand, word):
+    '''
+    This function gets as input the word which is played by the gamer and the hand dict, and removes the letters from the copie dictionary
+    we have made to not modifying the hand dictionary. If the item generated in the loop has the same value
+    than the same item in hand dictionary, then it is deleted. If not, then is substracted. The function return new_hand dictionary which
+    contents only the items still to play by the gamer.
+    '''
     copie = hand.copy()
     new_hand = dict()
     for i in word:
